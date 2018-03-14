ad39e74443e10c85587869436564437505e9ca5a
rockstar <puneetbindal8989@gmail.com>
2018-03-14 21:50:28 +0530

    made change

--- server.c
+++ server.c
@@ -7,12 +7,10 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
-
 struct sockaddr_in c_addr;
 char fname[100];
 int size=0;
 char buf[100];
-int server_fd, new_socket, valread;
 
 
 void* SendFileToClient(int *arg)
@@ -23,12 +21,12 @@ void* SendFileToClient(int *arg)
         write(connfd, fname,256);
 
         FILE *fp = fopen(fname,"rb");
-	 fseek(fp, 0, 2);    /* file pointer at the end of file */
-    	size = ftell(fp);
-	snprintf(buf, sizeof(buf), "%d", size);
-	write(connfd, buf,256);
-	fclose(fp);
-	fp = fopen(fname,"rb");
+        fseek(fp, 0, 2); /* file pointer at the end of file */
+        size = ftell(fp);
+        snprintf(buf, sizeof(buf), "%d", size);
+        write(connfd, buf,256);
+        fclose(fp);
+        fp = fopen(fname,"rb");
         if(fp==NULL)
         {
                 printf("File open error");
@@ -46,7 +44,7 @@ void* SendFileToClient(int *arg)
                 /* If read was success, send data. */
                 if(nread > 0)
                 {
-                        
+
                         write(connfd, buff, nread);
                 }
                 if (nread < 1024)
@@ -67,48 +65,64 @@ void* SendFileToClient(int *arg)
         sleep(2);
 }
 
-void connection_setup(){
-	  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+int main(int argc, char *argv[])
+{
+        int connfd = 0,err;
+        pthread_t tid;
+        struct sockaddr_in serv_addr;
+        int listenfd = 0,ret;
+        char sendBuff[1025];
+        int numrv;
+        size_t clen=0;
+
+        listenfd = socket(AF_INET, SOCK_STREAM, 0);
+        if(listenfd<0)
         {
-                perror("socket failed");
-                exit(EXIT_FAILURE);
+                printf("Error in socket creation\n");
+                exit(2);
         }
 
-        // Forcefully attaching socket to the port 8080
-        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                       &opt, sizeof(opt)))
-        {
-                perror("setsockopt");
-                exit(EXIT_FAILURE);
-        }
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons( PORT );
+        printf("Socket retrieve success\n");
 
-        // Forcefully attaching socket to the port 8080
-        if (bind(server_fd, (struct sockaddr *)&address,
-                 sizeof(address))<0)
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serv_addr.sin_port = htons(5000);
+
+        ret=bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
+        if(ret<0)
         {
-                perror("bind failed");
-                exit(EXIT_FAILURE);
+                printf("Error in bind\n");
+                exit(2);
         }
 
-        if (listen(server_fd, 3) < 0)
+        if(listen(listenfd, 10) == -1)
         {
-                perror("listen");
-                exit(EXIT_FAILURE);
-        }
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                                 (socklen_t*)&addrlen))<0)
-        {
-                perror("accept");
-                exit(EXIT_FAILURE);
+                printf("Failed to listen\n");
+                return -1;
         }
 
+        if (argc < 2)
+        {
+                printf("Enter file name to send: ");
+                gets(fname);
+        }
+        else
+                strcpy(fname,argv[1]);
 
+        while(1)
+        {
+                clen=sizeof(c_addr);
+                printf("Waiting...\n");
+                connfd = accept(listenfd, (struct sockaddr*)&c_addr,&clen);
+                if(connfd<0)
+                {
+                        printf("Error in accept\n");
+                        continue;
+                }
+                err = pthread_create(&tid, NULL, &SendFileToClient, &connfd);
+                if (err != 0)
+                        printf("\ncan't create thread :[%s]", strerror(err));
+        }
+        close(connfd);
+        return 0;
 }
-
-
-
-
-
